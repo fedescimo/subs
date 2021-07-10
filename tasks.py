038062a7bfb96ec448fcde0800d773c3d1dd7297
Luca Braglia <lbraglia@gmail.com>
2021-07-10 10:34:29 +0200

    invoke utils

--- tasks.py
+++ tasks.py
@@ -1,6 +1,9 @@
 from invoke import task
 from pyavsubs.Prj import Prj
 
+# -------------
+# Project tasks
+# -------------
 @task(default = True)
 def test(c):
     Prj(id = 'test', yt_id = 'Lox6tAor5Xo').menu()
@@ -13,10 +16,32 @@ def gymix(c):
 def arav(c):
     Prj(id = 'arav', yt_id = 'ZTlx0I4qDoo').menu()
 
+# @task
+# def hnva2(c):
+#     Prj(id = 'hnva2', yt_id = 'Jaok_8MNntQ').menu()
+
 @task
-def hnva2(c):
-    Prj(id = 'hnva2', yt_id = 'Jaok_8MNntQ').menu()
-    
+def hnva25(c):
+    Prj(id = 'hnva25', yt_id = 'Jaok_8MNntQ').menu()
+
+
+# -----
+# Utils
+# -----
 @task
 def autocomplete(c):
     c.run("source <(inv --print-completion-script bash)")
+
+@task
+def download_video(c):
+    url = input('Input YT video url: ')
+    cmd = "cd /tmp && youtube-dl -f 18 " + "'" + url + "'"
+    c.run(cmd)
+    print("Video downloaded in /tmp/")
+
+@task
+def download_subs(c):
+    url = input('Input YT video url: ')
+    cmd = "cd /tmp && youtube-dl --skip-download --all-subs --sub-format srt " + "'" + url + "'"
+    c.run(cmd)
+    print("Subs downloaded in /tmp/")
